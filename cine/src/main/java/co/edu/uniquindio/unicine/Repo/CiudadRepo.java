85fc864f9bdd005c80f5e7d1af537716f6f54302
Andres-Shadow <andresm.dussanb@uqvirtual.edu.co>
2022-10-08 20:15:50 -0500

    correciones 1

--- cine/src/main/java/co/edu/uniquindio/unicine/Repo/CiudadRepo.java
+++ cine/src/main/java/co/edu/uniquindio/unicine/Repo/CiudadRepo.java
@@ -2,8 +2,14 @@ package co.edu.uniquindio.unicine.Repo;
 
 import co.edu.uniquindio.unicine.Entidades.Ciudad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CiudadRepo extends JpaRepository<Ciudad, Integer> {
+
+    @Query("select c from Ciudad c join c.teatros t")
+    List<Object[]> contarTeatros();
 }
--- cine/src/main/java/co/edu/uniquindio/unicine/Repo/CompraRepo.java
+++ cine/src/main/java/co/edu/uniquindio/unicine/Repo/CompraRepo.java
@@ -2,8 +2,13 @@ package co.edu.uniquindio.unicine.Repo;
 
 import co.edu.uniquindio.unicine.Entidades.Compra;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CompraRepo extends JpaRepository<Compra, Integer> {
+
+
 }
