e1aaa87fb5031260c9aa82c69404c5c3b39d5e71
Andres-Shadow <andresm.dussanb@uqvirtual.edu.co>
2022-10-30 15:23:33 -0600

    registro cliente +-

--- web/src/main/java/co/edu/uniquindio/unicine/bean/InicioBean.java
+++ web/src/main/java/co/edu/uniquindio/unicine/bean/InicioBean.java
@@ -10,14 +10,5 @@ import java.io.Serializable;
 @ViewScoped
 @Component
 public class InicioBean implements Serializable {
-    @Getter @Setter
-    private String mensaje = "MAJO GOD <3";
-    @Getter @Setter
-    private String dato1, dato2;
 
-    public void cambiarValores(){
-        String aux = dato1;
-        dato1 = dato2;
-        dato2 = aux;
-    }
 }
